b9be2f4545ffac9d06dbb937ac26d64685affc34
Mikhail Karmachenko <carmachenko@yandex.ru>
2022-12-13 07:28:10 +0300

    123

--- Lesson84n/js/script.js
+++ Lesson84n/js/script.js
@@ -235,16 +235,32 @@ window.addEventListener('DOMContentLoaded', function () {
             const request = new XMLHttpRequest();
 
             request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'multipart/form-data');
+            //Очень важно!!!
+            //при использовании XMLHttpRequest и formData - заголовок устанавливать не нужно
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            
             
             const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach((value,key) => {
+                object[key] = value;
+            }); 
+
+            const json = JSON.stringify(object);
             
-            request.send(formData);
+            request.send(json);
 
             request.addEventListener('load', () => {
                 if (request.status === 200){
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
                 } else {
                     statusMessage.textContent = message.failure;
                 }
